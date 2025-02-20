81e76f6f91a2314d8338a5e1f4df0991304d7826
RizkiEka <reka48644@gmail.com>
2025-02-20 11:58:09 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\KasirController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ManajerController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::get('/register', [RegisterController::class, 'showRegister'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('register.process');
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.process');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+// Role -> Admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard-admin', [AdminController::class, 'index'])->name('dashboard.admin');
+});
+
+// Role -> Kasir
+Route::middleware(['auth', 'role:kasir'])->group(function () {
+    Route::get('/dashboard-kasir', [KasirController::class, 'index'])->name('dashboard.kasir');
+});
+
+// Role -> Manajer
+Route::middleware(['auth', 'role:manajer'])->group(function () {
+    Route::get('/dashboard-manajer', [ManajerController::class, 'index'])->name('dashboard.manajer');
+});
+
+
+Route::get('/unauthorized', function () {
+    return view('errors.unauthorized');
 });
